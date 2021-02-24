b80acd9b6bed942026fa8549a7ddaadf635840e8
yinghongbin <yinghongbin@appledeMacBook-Pro.local>
2021-02-24 11:18:13 +0800

    1

--- ch04/mtsleepA.py
+++ ch04/mtsleepA.py
@@ -17,7 +17,7 @@ def main():
     print 'starting at:', ctime()
     thread.start_new_thread(loop0, ())
     thread.start_new_thread(loop1, ())
-    sleep(6)
+    
     print 'all DONE at:', ctime()
 
 if __name__ == '__main__':
